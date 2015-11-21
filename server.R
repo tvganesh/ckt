8a6f51ed46eff78b09194ef3fa1b6c761098cdd6
Tinniam V Ganesh <tvganesh.85@gmail.com>
2015-11-21 16:27:58 +0530

    Fixed order of dropdown

--- server.R
+++ server.R
@@ -16,12 +16,23 @@ tendulkar <- read.csv("tendulkar.csv")
 print(dim(tendulkar))
 
 players <- c("tendulkar","dravid")
-funcs <- c("4s of batsman","6s of batsman","4s & 6s of batsman","Ground avg of batsman",
-            "Avg runs scored vs opposition",
-           "Contribution to wins & losses","Batsman's dismissals","batsmanMeanStrikeRate","batsmanMovingAverage",
-           "batsmanPerfBoxHist",'batsmanPerfForecast',"batsmanPerfHomeAway","batsmanRunsFreqPerf",
-           "batsmanRunsLikelihood","batsmanRunsPredict","batsmanRunsRanges","battingPerf3d",
-           "checkBatsmanInForm")
+funcs <- c("4s of batsman","6s of batsman",
+           "4s & 6s of batsman",
+           "Batsman's dismissals",
+           "Batsman's Mean Strike Rate",
+           "Batsman Runs-Freq. chart",
+           "Batsman Run Ranges",
+           "Batsman's Perf BoxHist chart",
+           "Ground avg of batsman",
+           "Avg runs scored vs opposition",
+           "Contribution to wins & losses",
+           "Batsman's Home-Away record",
+           "Batsman's Moving Average",
+           'Batsman Perf. Forecast',
+           "Batsman Runs Likelihood est.",
+           "Batsman Perf. 3-D",
+           "Check batsmanI form status")
+
 func1 <- c("bowlerAvgWktsGround","bowlerAvgWktsOpposition","bowlerContributionWonLost","bowlerEconRate",
            "bowlerHistWickets","bowlerMovingAverage","bowlerPerfForecast","bowlerPerfHomeAway",
            "bowlerWktRateTT","checkBowlerInForm")
@@ -45,11 +56,7 @@ shinyServer(function(input, output,session) {
         file <- paste(input$player,".csv",sep="")
         filesp <- paste(input$player,"sp.csv",sep="")
         cat(file,"\n")
-        funcs <- c("4s of batsman","6s of batsman","batsman4s6s","batsmanAvgRunsGround","batsmanAvgRunsOpposition",
-                   "batsmanContributionWonLost","batsmanDismissals","batsmanMeanStrikeRate","batsmanMovingAverage",
-                   "batsmanPerfBoxHist",'batsmanPerfForecast',"batsmanPerfHomeAway","batsmanRunsFreqPerf",
-                   "batsmanRunsLikelihood","batsmanRunsPredict","batsmanRunsRanges","battingPerf3d",
-                   "checkBatsmanInForm")
+       
         if(input$func =="4s & 6s of batsman"){
             batsman4s6s(file,"tendulkar")
         } else if (input$func == "4s of batsman"){
@@ -64,27 +71,25 @@ shinyServer(function(input, output,session) {
             batsmanContributionWonLost(filesp,"Tendulkar")
         } else if (input$func == "Batsman's dismissals"){
             batsmanDismissals(file,"Tendulkar")
-        } else if (input$func == "batsmanMeanStrikeRate"){
+        } else if (input$func == "Batsman's Mean Strike Rate"){
             batsmanMeanStrikeRate(file,"Tendulkar")
-        } else if (input$func == "batsmanMovingAverage"){
+        } else if (input$func == "Batsman's Moving Average"){
             batsmanMovingAverage(file,"Tendulkar")
-        }  else if (input$func == "batsmanPerfBoxHist"){
+        }  else if (input$func == "Batsman's Perf BoxHist chart"){
             batsmanPerfBoxHist(file,"Tendulkar")
-        }  else if (input$func == "batsmanPerfForecast"){
+        }  else if (input$func == "Batsman Perf. Forecast"){
             batsmanPerfForecast(file,"Tendulkar")
-        } else if (input$func == "batsmanPerfHomeAway"){
+        } else if (input$func == "Batsman's Home-Away record"){
             batsmanPerfHomeAway(filesp,"Tendulkar")
-        }  else if (input$func == "batsmanRunsFreqPerf"){
+        }  else if (input$func == "Batsman Runs-Freq. chart"){
             batsmanRunsFreqPerf(file,"Tendulkar")
-        } else if (input$func == "batsmanRunsLikelihood"){
+        } else if (input$func == "Batsman Runs Likelihood est."){
             batsmanRunsLikelihood(file,"Tendulkar")
-        }  else if (input$func == "batsmanRunsRanges"){
+        }  else if (input$func == "Batsman Run Ranges"){
             batsmanRunsRanges(file,"Tendulkar")
-        }   else if (input$func == "batsmanRunsPredict"){
-            batsmanRunsPredict(file,"Tendulkar")
-        }   else if (input$func == "battingPerf3d"){
+        }  else if (input$func == "Batsman Perf. 3-D"){
             battingPerf3d(file,"Tendulkar")
-        }   else if (input$func == "checkBatsmanInForm"){
+        }   else if (input$func == "Check batsmanI form status"){
             checkBatsmanInForm(file,"Tendulkar")
         } else if (input$func == "batsman6s"){
             batsman6s(file,"Tendulkar")
